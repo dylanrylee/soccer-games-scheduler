a29c45b30fcdf8959c3c7a26bd87155a5ceb98f4
KeenanHB <khb.hanbal@gmail.com>
2024-11-29 22:29:06 -0700

    Update search_process.py

--- search_process.py
+++ search_process.py
@@ -7,26 +7,50 @@ class SearchProcess:
     """
     Handles the search process for finding feasible scheduling solutions.
     """
-    def __init__(self, root: AndTreeNode, constraints: HardConstraints):
+    def __init__(self, root: AndTreeNode, hard_constraints: HardConstraints, soft_constraints: SoftConstraints):
         self.root = root
-        self.constraints = constraints
+        self.hard_constraints = hard_constraints
+        self.soft_constraints = soft_constraints
+    
+    """
+    Decides which of the AndTreeNodes children to expand and expands the AndTree Node
+    """
+    def f_leaf(self, games: List[Game], practices: List[Practice]) -> Union[Game, Practice]:
+        
+        best_game = Game
+        best_practice =  Practice
+        
+        "Eval is what is used to find score based from the soft constraints"
+        for AndTreeNode in self.root.children:
+            for game in games:
+                if Eval(self, game) > Eval(self, best_game):
+                    best_game = game
+                elif Eval(self, game) == Eval(self, best_game):
+                    best_game = f_select(self, game, best_game)
+            for practice in practices:
+                if Eval(practice) > Eval(best_practice):
+                    best_practice = practice
+                elif Eval(practice) == Eval(best_practice):
+                    best_practice = f_select(self, practice, best_practice)
+        
+        if Eval(self, best_game) > Eval(self, best_practice):
+            self.root.expand(best_game)
+        elif Eval(self, best_game) < Eval(self, best_practice):
+            self.root.expand(best_practice)
+        else: 
+            f_select(self, best_game, best_practice)
 
-    def depth_first_search(self):
-        """
-        Perform a depth-first search to find a valid schedule.
-        """
-        stack = [self.root]
-        while stack:
-            current_node = stack.pop()
-            if self.constraints.constr():
-                print("Solution found!")
-                return current_node
+    """
+    
+    """
+    def f_select(self, option_1: Union[Game, Practice],option_2 :Union[Game, Practice]) -> Union[Game, Practice]:
+        
+        return
+    
+    
+    """
+    
+    """
+    def f_bound(self):
 
-            current_node.expand(self.constraints)
-            stack.extend(current_node.children)
-
-        print("No valid solution found.")
-        return None
-
-    def __repr__(self):
-        return f"SearchProcess(Root={self.root})"
+        return
